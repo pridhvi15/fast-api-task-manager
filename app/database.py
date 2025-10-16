4b6d67dbfddb0b4d6c056c7f0f491cb4557eb2e8
Pridhvi <pridhvi>
2025-10-16 20:39:39 +0530

    changed database

--- app/database.py
+++ app/database.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set in .env")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
